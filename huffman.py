d5481294a14c673b9a37297d1d91c0d42e31db27
randylopez <logentix@gmail.com>
2019-03-24 23:56:18 -0400

    huffman tree working now

--- huffman.py
+++ huffman.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+from binarytree import build
 
 class Stack:
     def __init__(self):
@@ -16,30 +16,62 @@ class Stack:
 
 
 file = "probabilities.txt"
-probabilities = {}  # a dictionary to hold codewords and their item
+probabilities = []
 
-# read probabilities from probabilities.txt
 with open(file) as fp:
     line = fp.readline()
     count = 0
     while line:
-        line = line.replace('\n', '')
-        if line != '':
-            if line in probabilities:
-                n = probabilities[line]
-                n += 1
-                probabilities[line] = n
-            else:
-                probabilities[line] = 1
+        line = line.replace('\n', '').strip()
+        if line: 
+            #print("line %s" % line)
+            prob = float(line)
+            probabilities.append(prob)
         line = fp.readline()
         count += 1
 
-print(probabilities)
+probabilities = sorted(probabilities, key=float, reverse=True)
+stack = Stack()
+stack.items = probabilities
+print(stack.items)
+tree = []
 
-stack = Stack()  # this will keep track of the two lowest values
 
-p_sorted = sorted(probabilities, key=probabilities.get, reverse=True)
+for item in stack.items:
+    # pop the smallest two 
+    n0 = stack.pop()
+    n1 = stack.pop()
+    n3 = n0 + n1
 
-print(p_sorted)
+    if(n3 < 1.0):
+        if(n3 > stack.items[0]):
+            insert_at = 0
+            b = stack.items[:] 
+            b[insert_at:insert_at] = [n3]
+            stack.items = b
 
-print("stack: " + str(stack.items))
+            print(stack.items)
+            print("..[%f , %f]" % (n0, n1))
+            tree.append([n0,n1])
+            #tree.append(stack.items)
+        else: 
+            insert_at = 1
+            b = stack.items[:] 
+            b[insert_at:insert_at] = [n3]
+            stack.items = b
+            print(stack.items)
+            print("...[%f , %f]" % (n0, n1))
+            tree.append([n0,n1])
+            #tree.append(stack.items)
+
+#append leftovers
+tree.append([stack.items[0]])
+print(tree)
+tree = [j for sub in tree for j in sub]
+tree = tree[::-1]
+print(tree)
+# tree = sorted(tree, reverse=True)
+root = build(tree)
+print(root)
+
+
